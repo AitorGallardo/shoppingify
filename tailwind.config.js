68f727db7301ab45ec4f5d2c12a786a682d9367e
aitor.gallardo <aitor.gallardo@apartool.com>
2023-09-11 11:19:06 +0200

    tailwing/flowbite with primary color config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F9A109', 
+      },
+    },
+  },
+  plugins: [
+    require('flowbite/plugin') // add this line
+  ],  
+}
