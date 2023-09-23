a913d2b3c45b76c72c44def6672df4d85200a998
aitor.gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-09-23 23:02:47 +0200

    working on not rerendering list on change state

--- src/app/store/selectors/grocery.selectors.ts
+++ src/app/store/selectors/grocery.selectors.ts
@@ -1,9 +1,7 @@
-// counter.selectors.ts
-
 import { createSelector } from '@ngrx/store';
 import { AppState } from '../app.reducers';
 
-// Select the counter property from the state
+// Select the groceries from the state
 export const selectAllGroceries = (state: AppState) => state.groceries.allGroceries;
 export const selectSelectedGroceries = (state: AppState) => state.groceries.selectedGroceries;
 
@@ -12,7 +10,14 @@ export const selectAllGroceriesValue = createSelector(
     selectAllGroceries,
   (allGroceries) => allGroceries
 );
+
 export const selectSelectedGroceriesValue = createSelector(
   selectSelectedGroceries,
   (selectedGroceries) => selectedGroceries
+);
+
+// Create a selector that retrieves a grocery by id
+export const selectGroceryById = (id: number) => createSelector(
+  selectSelectedGroceries,
+  (selectedGroceries) => selectedGroceries.find(grocery => grocery.id === id)
 );
